87352aefbc15ecb5ff85258fb26016c263346c64
ATREE Bangalore <70751764+atreebangalore@users.noreply.github.com>
2025-12-18 09:49:28 +0530

    new begining

--- app/components/NewBegining.tsx
+++ app/components/NewBegining.tsx
@@ -6,18 +6,21 @@ const NewBegining = () => {
         <div className='flex flex-col gap-5 p-5 '>
             <Heading text='A New Beginning' />
             <ul className="list-disc px-4  mb-4 space-y-2">
-                <li>A self-sustaining model takes shape at Venkateshpura Lake.</li>
-                <li>Sustained community-driven efforts transform the lake from a neglected waterbody into a vibrant public space.</li>
-                <li>The lake attracts considerable daily footfall from the community. Even at noon, women can be seen sitting on the benches, enjoying the afternoon breeze.</li>
-                <li>The lake forum is well-informed about potential pollution sources and knows whom to alert during issues like algal blooms.</li>
-                <li>Members regularly document and share photos of birds, sunrises and sunsets, building pride and a sense of connection.</li>
-                <li>Through the lake trust, residents take charge of the emerging challenges.</li>
-                <li>The stage is set for a long-term, community-driven model of lake restoration.</li>
-
-
-
+                <li> The Pied Kingfisher has returned to Venkateshpura Lake.</li>
+                <li>Watching it hover over the lake before diving to fish gives a sense of hope.</li>
+                <li>After restoration, biodiversity assessments showed a mild dip in some species.</li>
+                <li>These changes were short-term responses to habitat reformation, and the system soon stabilised.</li>
+                <li>Residential areas around the lake have not needed water tankers for over a year, showing the lake is functioning as an aquifer.</li>
+                <li>Walkers of all ages and enthusiastic birders now regularly visit the lake.</li>
+                <li>The lake has become a safe space for women to sit and relax after work on hot afternoons.</li>
+                <li>The water appears clearer, and water quality metrics indicate improvement, though the work is not complete.</li>
+                <li>Water quality must remain consistent, especially during summer when receding water can cause dangerously low dissolved oxygen levels.</li>
+                <li>Floating islands and newly introduced plants require regular maintenance.</li>
+                <li>Preventing illegal drainage into the lake and protecting the fragile historical GTS structure requires constant vigilance.</li>
+                <li>The fencing around the restored lake has unfortunately excluded some key stakeholders, such as migrants and pastoralists.</li>
+                <li>Restoration is not an endpoint but an ongoing process that needs continuous upkeep and shared responsibility.</li>
             </ul>
-        </div>
+        </div >
     )
 }
 
