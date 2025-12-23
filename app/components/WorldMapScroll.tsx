8ec639d0bfbd659c889054d36066d0312f816fe5
ATREE Bangalore <70751764+atreebangalore@users.noreply.github.com>
2025-12-23 22:58:58 +0530

    updated236

--- app/components/WorldMapScroll.tsx
+++ app/components/WorldMapScroll.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import React, { useRef, useState } from 'react'
+import useTailwindBreakpoint from '../hooks/useTailwindBreakpoint';
 
 const WorldMapScroll = () => {
 
@@ -8,7 +9,30 @@ const WorldMapScroll = () => {
     const [transform, setTransform] = useState("scale(1) translate(0px, 0px)");
     const [iszoomed, setIsZoomed] = useState(false);
 
-    const zoomToMarker = async () => {
+    const [bp, setBp] = useState('sm');
+
+    const countryList: { [key: string]: any } = {
+        "El Salvador": {
+            name: "El Salvador",
+            sm: { x: 90, y: 0 },
+            md: { x: 140, y: 5 },
+            lg: { x: 180, y: 0 },
+            xl: { x: 220, y: 10 },
+            xl2: { x: 286, y: 15 }
+        },
+        "Brazil": {
+            
+            sm: { x: 200, y: 100 },
+            md: { x: 260, y: 130 },
+            lg: { x: 320, y: 160 },
+            xl: { x: 400, y: 200 },
+            xl2: { x: 480, y: 240 }
+        },
+
+        // Add more countries as needed
+    };
+
+    const zoomToMarker = async (country:string) => {
 
         if (iszoomed) {
             // Reset zoom
@@ -20,8 +44,58 @@ const WorldMapScroll = () => {
 
         const scale = 3;
 
-        const translateX = 276;
-        const translateY = 15;
+        const update = () => {
+            const w = window.innerWidth;
+
+            if (w >= 1536) setBp('2xl');
+            else if (w >= 1280) setBp('xl');
+            else if (w >= 1024) setBp('lg');
+            else if (w >= 768) setBp('md');
+            else setBp('sm');
+        };
+
+
+
+        update();        window.addEventListener('resize', update);
+        let translateX;
+        let translateY;
+
+        switch (bp) {
+            case "sm":
+                translateX = countryList[country].sm.x;
+                translateY = countryList[country].sm.y;
+                break;
+            case "md":
+                translateX = countryList[country].md.x;
+                translateY = countryList[country].md.y;
+                break;
+            case "lg":
+                translateX = countryList[country].lg.x;
+                translateY = countryList[country].lg.y;
+                break;
+            case "xl":
+                translateX = countryList[country].xl.x;
+                translateY = countryList[country].xl.y;
+                break;
+            case "2xl":
+                translateX = countryList[country].xl2.x;
+                translateY = countryList[country].xl2.y;
+                break;
+            case "base":
+                translateX = countryList[country].sm.x;
+                translateY = countryList[country].sm.y;
+                // Handle small screens
+                break;
+
+            default:
+                break;
+        }
+
+        // const translateX = 276;
+        // const translateY = 15;
+
+        // const translateX = 90;
+        // const translateY = 0;
 
         setTransform(
             `scale(${scale}) translate(${translateX}px, ${translateY}px)`
@@ -54,9 +128,21 @@ const WorldMapScroll = () => {
             {/* Marker */}
             <div
 
-                onClick={zoomToMarker}
-                className="absolute w-5 h-5 bg-yellow-300 rounded-full  opacity-0
-                   cursor-pointer top-29 left-35 xl:top-83 xl:left-103  border-4 border-white "
+                onClick={() => zoomToMarker("El Salvador")}
+                className="absolute w-15 h-15 bg-yellow-300 rounded-full  opacity-0
+                   cursor-pointer top-25 left-25 md:top-35 md:left-40 lg:top-45 lg:left-55 
+                    xl:top-60 xl:left-73  border-4 border-white "
+            />
+
+            {/* Marker */}
+            <div
+
+                onClick={() => zoomToMarker("Brazil")}
+                className="absolute w-15 h-15 bg-green-300 
+                xl:h-40 xl:w-40 hidden
+                rounded-full  opacity-100
+                   cursor-pointer top-25 left-25 md:top-35 md:left-40 lg:top-45 lg:left-55 
+                    xl:top-75 xl:left-90  border-4 border-white "
             />
 
 
@@ -115,7 +201,7 @@ const WorldMapScroll = () => {
                     </div>
                     <div>
                         <button
-                            onClick={zoomToMarker}
+                            onClick={() => zoomToMarker("")}
                             className=" items-center w-auto px-4 py-2 text-sm font-medium rounded-lg bg-[#087f9b] text-white hover:bg-[#056379] transition cursor-pointer"
                         >
                             ← Back to world
